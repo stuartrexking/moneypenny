300b581bdeef55db285416171f0993b6908fc03b
Stuart King <stuartrexking@gmail.com>
2015-03-17 13:06:33 +1000

    Run packer with packer user. Update README

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+useradd -d /home/vagrant -s /bin/bash vagrant 
+
 groupadd admin
 usermod -G admin vagrant
 echo '%admin ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
@@ -8,17 +10,16 @@ echo 'UseDNS no' >> /etc/ssh/sshd_config
 
 mkdir -p /home/vagrant/.ssh/
 chmod 700 /home/vagrant/.ssh
-cd ~/.ssh
 wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' -O /home/vagrant/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
+chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 apt-get update
 apt-get -y install linux-headers-$(uname -r) dkms
 
 mkdir /tmp/virtualbox
-VERSION=$(cat /home/vagrant/.vbox_version)
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
+VERSION=$(cat /home/packer/.vbox_version)
+mount -o loop /home/packer/VBoxGuestAdditions_$VERSION.iso /tmp/virtualbox
 sh /tmp/virtualbox/VBoxLinuxAdditions.run
 umount /tmp/virtualbox
 rmdir /tmp/virtualbox
